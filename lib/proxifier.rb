2a5073d5aca4751875eb9938ce4e67d693607f3e
Bartek Głowacki <marathone@gmail.com>
2016-11-05 20:01:10 +0100

    Pass proxy_source to a runner

--- lib/proxifier.rb
+++ lib/proxifier.rb
@@ -5,20 +5,22 @@ require 'proxifier/conf_generator'
 require 'proxifier/conf_saver'
 
 module Proxifier
- class Runner
-   def initialize
-     @downloader = InCloak::Downloader.new
-     @parser = InCloak::Parser.new
-     @conf_saver = ConfSaver.new
-   end
+  class Runner
+    def initialize(proxy_source = "InCloak")
+      if proxy_source == "InCloak"
+        @downloader = InCloak::Downloader.new
+        @parser = InCloak::Parser.new
+      end
+      @conf_saver = ConfSaver.new
+    end
 
-   def call
-     page = @downloader.call
-     proxies = @parser.call(page).map(&:to_s)
-     configuration = ConfGenerator.new(proxies).render
-     @conf_saver.save(configuration)
-   end
- end
+    def call
+      page = @downloader.call
+      proxies = @parser.call(page).map(&:to_s)
+      configuration = ConfGenerator.new(proxies).render
+      @conf_saver.save(configuration)
+    end
+  end
 end
 
 runner = Proxifier::Runner.new
